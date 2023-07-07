6ddd144c0977c9b81b9daeae2e5d075fbf803596
Carlos Masson <carlosmasson96@gmail.com>
2023-07-06 21:54:23 -0400

    testing emailing

--- src/app/sections/contact.js
+++ src/app/sections/contact.js
@@ -47,7 +47,7 @@ const Contact = () => {
           </div>
         </div>
         <form action="#" method="POST" name="contact" data-netlify="true" className="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48">
-        <input type="hidden" name="form-name" value="the-name-of-the-html-form" />
+        <input type="hidden" name="form-name" value="contact" />
           <div className="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
             <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
               <div>
