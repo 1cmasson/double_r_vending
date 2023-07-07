ab0ba27c2a1ef98f7ee73ae2f1d3aab72756af8c
Carlos Masson <carlosmasson96@gmail.com>
2023-07-06 21:28:34 -0400

    netlify forms

--- src/app/sections/contact.js
+++ src/app/sections/contact.js
@@ -46,7 +46,7 @@ const Contact = () => {
             </dl>
           </div>
         </div>
-        <form action="#" method="POST" name="contact" className="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48">
+        <form action="#" method="POST" name="contact" data-netlify="true" className="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48">
         <input type="hidden" name="form-name" value="the-name-of-the-html-form" />
           <div className="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
             <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
