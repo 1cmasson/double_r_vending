18fff32d79558ec9e58ac343c5a0c85fb3c2b907
Carlos Masson <carlosmasson96@gmail.com>
2023-09-06 22:51:17 -0400

    facebook_messenger

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Video from './sections/video';
 import Partners from './sections/partners';
 import Contact from './sections/contact';
 import Footer from './sections/footer';
+import { FacebookProvider, CustomChat } from 'react-facebook';
 // import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
@@ -12,7 +13,9 @@ const App = () => {
   return (
       <main>
         <Hero/>
-        <TawkToWrapper/>
+        <FacebookProvider appId="1729047264226575" chatSupport>
+          <CustomChat pageId="114420448381528" minimized={true}/>
+        </FacebookProvider> 
         <section id='about'>
           <Video/>
         </section>
